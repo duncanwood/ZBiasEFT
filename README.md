28e9ca1db62eb3c6171d4f4a24b68101bef3fe96
Duncan Wood <duncan.wood89@gmail.com>
2018-03-20 16:12:38 -0700

    Made README more descriptive

--- README.md
+++ README.md
@@ -3,4 +3,27 @@
 Installation
 ---------------------
 
-Configure Makefile to use a C++ compiler that implements the C++11 standard (gcc 4.8.1 and later)
+This software is dependent only on one library:
+https://github.com/jwgcarlson/Copter
+
+Install that library wherever is convenient, 
+and point your linker to that directory.
+If you install it in a default library location
+(e.g. found in `LD_LIBRRY_PATH`), you need only
+the `-lcopter` flag.
+
+Configure Makefile to use a C++ compiler that 
+implements the C++11 standard (g++ 4.8.1 and later)
+
+Then compile using `make`.
+
+Usage
+---------------------
+
+After compilation, calling `/.ZBiasEFT` will begin 
+the computation, and continuously store the results 
+in a file called `integrals_noresum.txt`
+
+#TODO: 
+add options
+
--- ZBiasEFT.cpp
+++ ZBiasEFT.cpp
@@ -1,54 +1,14 @@
 #include "mdefs.h"
-#include "vec3.h"
 #include <iostream>
 #include <fstream>
-#include <Copter/GrowthFunction.h>
 #include <Copter/InterpolatedPS.h>
 #include <Copter/Cosmology.h>
 #include <Copter/Quadrature.h>
 #include "kernels.h"
 #include "integrands.h"
-#include <unordered_map>
 #include <deque>
 
 using namespace std;
-
-
-int test1() {
-	vec3 v (1.,2.,3.);
-	vec3 w = v+v;
-	vec3 v1 (1, 2,  3);
-	vec3 v2 (5, -1, 2);
-	vec3 v3 (-1, 3, 4);
-	real coeffs [14] = {0,1.6104001172697386,1.3968722737867396,
-				1.1548119602404654,0.5986928441620107,
-				0.7877253272927272,1.0416740075755633,
-				1.18373842754725,1.049831274625591,
-				1.8579695766190194,1.029707660797118,
-				1.7534242467146504,1.0395350862065085,
-				1.4303489782311694};
-	/*
-	   for (int i = 1; i <= 4; i++) {
-	   cout << "c_2_" << i << "(v1,v2) = " << c2(i, v1,v2) << endl;
-	   }
-   */
-	for (int i = 1; i <= 12; i++) {
-		cout << "c_3_" << i << "(v1,v2,v3) = " << c3(i, v1,v2,v3) << endl;
-	}
-
-	cout << "K2(v1,v2,coeffs) = " << K2(v1,v2,coeffs) << endl;
-	cout << "K2(v2,v1,coeffs) = " << K2(v2,v1,coeffs) << endl;
-
-	cout << "K3(v1,v2,v3,coeffs) = " << K3(v1,v2,v3,coeffs) << endl;
-	cout << "K3(v1,v3,v2,coeffs) = " << K3(v1,v3,v2,coeffs) << endl;
-	cout << "K3(v2,v3,v1,coeffs) = " << K3(v2,v3,v1,coeffs) << endl;
-	cout << "K3(v2,v1,v3,coeffs) = " << K3(v2,v1,v3,coeffs) << endl;
-	cout << "K3(v3,v2,v1,coeffs) = " << K3(v3,v2,v1,coeffs) << endl;
-	cout << "K3(v3,v1,v2,coeffs) = " << K3(v3,v1,v2,coeffs) << endl;
-
-
-	return 0;
-}
 
 struct CachedValue {
 	real k;
@@ -56,7 +16,7 @@ struct CachedValue {
 	CachedValue(real new_k, real new_ps) : k(new_k), ps(new_ps) {}
 };
 
-static int testIntegrands(int argc, char** args) {
+static int computeIntegrals(int argc, char** args) {
 	
 	int nIntegrals = 1;
 	if (argc > 1) {
@@ -97,8 +57,6 @@ static int testIntegrands(int argc, char** args) {
 			0.5874276,0.5937501,0.6000223,0.6062977,
 			0.6125227,0.6188515,0.6251628,0.6313986};
 
-
-//	std::unordered_map<real,real> cache;
 	deque <CachedValue> cache;
 	for (int i = 0; i < 6; i++) {
 		cache.push_back(CachedValue(0,0));
@@ -117,14 +75,7 @@ static int testIntegrands(int argc, char** args) {
 		for (real k : klist) {
 			data << k << " " << 
 				Integrate<2>([&](real x, real q) { 
-					//std::cout << "cache size: " << cache.size() << endl;
 					return q*q/(Power(2*Pi,3))*Integrand(i, k, q, x, [&] (real kk) -> const real {
-						//if (cache.count(kk) == 0)
-						//	cache[kk] = ps.Evaluate(kk);
-						//return cache[kk];
-						
-						//return ps.Evaluate(kk);
-						//
 						int index = check_cache(cache, kk);
 						if (index == -1) {
 							cache.pop_back();
@@ -146,5 +97,5 @@ static int testIntegrands(int argc, char** args) {
 
 
 int main(int argc, char** args) {
-		return testIntegrands(argc, args);
+		return computeIntegrals(argc, args);
 }
